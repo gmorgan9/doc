7fbb79fc24f100fd9fcf12c08a4eef3f47186e69
Garrett Morgan <gar.mor.pro@outlook.com>
2022-02-18 09:48:23 -0700

    update

--- app/database/connect.php
+++ app/database/connect.php
@@ -9,6 +9,8 @@ $conn = new MySQLi($host, $user, $pass, $db_name);
 
 if ($conn->connect_error) {
     die('Database connection error: ' . $conn->connect_error);
+} else {
+    echo "DB connection successful";
 }
 
 ?>
